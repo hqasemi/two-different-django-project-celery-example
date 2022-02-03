ac89cc6f54899d53dfa2bd5208c7ba3fd4bc51b2
Hessam <s.hesam.ghasemi@gmail.com>
2022-02-03 18:03:31 +0330

    change `TaskSerializer` to serialize model properties as well as fields

--- producer_django_project/tasks/serializers.py
+++ producer_django_project/tasks/serializers.py
@@ -6,4 +6,13 @@ from tasks.models import TaskModel
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = TaskModel
-        fields = '__all__'
+        # fields = '__all__'
+        # Because (task_result and task_status) are not a model fields,
+        # it needs to add all the fields and properties explicitly to the serializer class
+        fields = (
+            "task_name",
+            "task_id",
+            "is_revoked",
+            "task_result",
+            "task_status",
+        )
